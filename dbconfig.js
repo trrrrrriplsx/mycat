0a36b3a1603b47381ca1ca06ac4e1f723e142efb
skrrr <katakum9@gmail.com>
2025-11-13 09:57:55 +0300

    Add files via upload

---
+++ dbconfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-project.firebaseapp.com",
+  databaseURL: "https://your-project-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "your-project",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abc123"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
